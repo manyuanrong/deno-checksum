ba8de9117f3b5b2bec062186c956c9fdeb4db374
Enok <416828041@qq.com>
2019-09-10 11:59:20 +0800

    upgrade deno version & fix testing deps

--- sha1.test.ts
+++ sha1.test.ts
@@ -1,31 +1,30 @@
+import { encode } from "https://deno.land/std/strings/encode.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { test } from "https://deno.land/std/testing/mod.ts";
 import { Hash, hex } from "./hash.ts";
-import { encode } from "https://deno.land/x/std/strings/strings.ts";
-import { test, runTests, assert } from "https://deno.land/x/testing/mod.ts";
 
 const hash = new Hash("sha1");
 
-test(function test1() {
+test(function testSha1Num() {
   const result = hash.digest(encode("123"));
-  assert.equal("40bd001563085fc35165329ea1ff5c5ecbdbbeef", result.hex());
+  assertEquals("40bd001563085fc35165329ea1ff5c5ecbdbbeef", result.hex());
   console.log(result.hex());
 });
 
-test(function test2() {
+test(function testSha1Chars() {
   const result = hash.digest(encode("abc"));
-  assert.equal("a9993e364706816aba3e25717850c26c9cd0d89d", result.hex());
+  assertEquals("a9993e364706816aba3e25717850c26c9cd0d89d", result.hex());
   console.log(result.hex());
 });
 
-test(function test3() {
+test(function testSha1Unicode() {
   const result = hash.digest(encode("我爱你"));
-  assert.equal("5890a73fed38bf09622c34ad9391f1d09c0ec100", result.hex());
+  assertEquals("5890a73fed38bf09622c34ad9391f1d09c0ec100", result.hex());
   console.log(result.hex());
 });
 
-test(function test4() {
+test(function testSha1ToHex() {
   const result = hex(hash.digest(encode("hex")).data);
-  assert.equal("7994ebae30a63934992a16deca856d50596bc1a9", result);
+  assertEquals("7994ebae30a63934992a16deca856d50596bc1a9", result);
   console.log(result);
 });
-
-runTests();
---
+++ test.ts
@@ -0,0 +1,5 @@
+import { runTests } from "https://deno.land/std/testing/mod.ts";
+import "./md5.test.ts";
+import "./sha1.test.ts";
+
+runTests();
