3c2598dd64218df13e0f5a8448020111fd0af3dd
Enok <416828041@qq.com>
2020-05-14 09:19:33 +0800

    bump to std@v0.50.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export {
   decode,
   encode,
-} from "https://deno.land/std@v0.42.0/encoding/utf8.ts";
+} from "https://deno.land/std@v0.50.0/encoding/utf8.ts";
 export {
   assertEquals,
-} from "https://deno.land/std@v0.42.0/testing/asserts.ts";
+} from "https://deno.land/std@v0.50.0/testing/asserts.ts";
